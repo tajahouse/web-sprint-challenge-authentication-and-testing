633077a9787e4e3ea30f2652822443e740336318
tajahouse <ahlliahtaja@yahoo.com>
2020-08-15 09:51:13 -0700

    Completed set up for middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,7 +1,31 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt  = require("jsonwebtoken");
+
+function restrict() {
+  return async (req, res, next) => {
+    const errMessage = (err, messageStatement) => {
+      res.status(err).json({
+        message: messageStatement,
+      });
+    };
+    try {
+       const token = req.headers.authorization;
+      if (!token) {
+        return errMessage(401, "Invalid Credentials");
+      }
+
+      jwt.verify(token, "all mine, not yours", (err, decoded) => {
+        if (err) {
+          return errMessage(401, "Invalid Credentials");
+        }
+        next();
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+}
+
+module.exports = restrict;
 
 module.exports = (req, res, next) => {
   res.status(401).json({ you: 'shall not pass!' });
