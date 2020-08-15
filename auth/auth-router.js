152a5bb6dea6a02d387eda6fe8aecd8cc82c1f6d
tajahouse <ahlliahtaja@yahoo.com>
2020-08-15 09:49:02 -0700

    Completed auth-model and importing depencencies to auth router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const auth = require('./auth-model')
+const restrict = require('./authenticate-middleware')
 
 router.post('/register', (req, res) => {
   // implement registration
+  
 });
 
 router.post('/login', (req, res) => {
